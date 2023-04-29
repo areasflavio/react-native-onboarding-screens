ae491e5871c7feca6216f8be26f838f7b48fdb91
Flávio Arêas <vflavio9@gmail.com>
2023-04-29 11:32:15 -0300

    feat: create screens animation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import {
-  FlatList,
-  Image,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -9,33 +7,118 @@ import {
   View,
 } from 'react-native';
 
+import Animated, {
+  Extrapolate,
+  interpolate,
+  SharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 import { data, type Data } from './src/data/screens';
 
-export default function App() {
+const RenderItem = ({
+  item,
+  index,
+  x,
+}: {
+  item: Data;
+  index: number;
+  x: SharedValue<number>;
+}) => {
   const { width: SCREEN_WIDTH } = useWindowDimensions();
 
-  const renderItem = ({ item, index }: { item: Data; index: number }) => (
-    <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
-      <Image
-        source={item.image}
-        style={{ width: SCREEN_WIDTH * 0.8, height: SCREEN_WIDTH * 0.8 }}
-      />
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    const opacityAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [0, 1, 0],
+      Extrapolate.CLAMP
+    );
 
-      <View>
+    const translateYAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [100, 0, 100],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      width: SCREEN_WIDTH * 0.8,
+      height: SCREEN_WIDTH * 0.8,
+      opacity: opacityAnimation,
+      transform: [{ translateY: translateYAnimation }],
+    };
+  });
+
+  const textAnimatedStyle = useAnimatedStyle(() => {
+    const opacityAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [0, 1, 0],
+      Extrapolate.CLAMP
+    );
+
+    const translateYAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [100, 0, 100],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      opacity: opacityAnimation,
+      transform: [{ translateY: translateYAnimation }],
+    };
+  });
+
+  return (
+    <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
+      <Animated.Image source={item.image} style={imageAnimatedStyle} />
+
+      <Animated.View style={textAnimatedStyle}>
         <Text style={styles.itemTitle}>{item.title}</Text>
         <Text style={styles.itemText}>{item.text}</Text>
-      </View>
+      </Animated.View>
     </View>
   );
+};
+
+export default function App() {
+  const x = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event, context) => {
+      x.value = event.contentOffset.x;
+    },
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
 
-      <FlatList
+      <Animated.FlatList
         data={data}
         keyExtractor={(item) => String(item.id)}
-        renderItem={({ item, index }) => renderItem({ item, index })}
+        renderItem={({ item, index }) => (
+          <RenderItem index={index} item={item} x={x} />
+        )}
+        onScroll={onScroll}
         scrollEventThrottle={16}
         horizontal
         showsHorizontalScrollIndicator={false}
